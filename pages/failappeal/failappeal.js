99691f8b3096cc46ecc8a48dea968c45fb51fd72
zhongxinyun <55012647+zhongxinyun@users.noreply.github.com>
2023-03-20 21:48:06 +0800

    完成人脸认证失败申诉页面

---
+++ pages/failappeal/failappeal.js
@@ -0,0 +1,68 @@
+const app = getApp()
+var globaldata = app.globalData;
+var util = require('../../utils/util');
+var date = util.formatDate(new Date());
+Page({
+    data: {
+        username: null,
+        staffID: null,
+        morning_flag: null,
+        date: null,
+        morning_str: '上午',
+        reason: '',
+    },
+    onLoad(options) {
+      var userinfo = wx.getStorageSync('userinfo')
+      this.setData({
+        morning_flag: options.morning_flag,
+        date: date,
+        username: userinfo.username,
+        staffID: userinfo.staffID
+      })
+      if(!this.data.morning_flag)
+        this.setData({
+          morning_str: '下午'
+        })
+      else
+        this.setData({
+          morning_str: '上午'
+        })
+    },
+    inputRemark:function(option){
+      let value = option.detail.value;
+      this.setData({
+        reason: value
+      })
+    },
+    formSubmit: function(e){
+      let vm = this;
+      wx.request({
+        url:  globaldata.serverHost + 'appeal/submit',
+        data: { 
+          date: vm.data.date,
+          staffID: vm.data.staffID,
+          appeal_reason: vm.data.reason,
+          morning_flag: vm.data.morning_flag,
+          category: 0
+        },
+        header: {
+          "Content-Type": "application/json"
+        },
+        method: 'POST',
+        dataType:'json',
+          success: function (res) {
+            wx.showToast({
+              title: '提交成功',
+              duration:2000,
+              mask:true,
+              icon:'success',
+            })
+            setTimeout(function () {
+              wx.switchTab({
+                url: '../main/main'
+              })
+            }, 2000);
+          }
+        })
+    },
+})
