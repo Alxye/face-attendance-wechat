f5ae13fc9d6f33a2d2583589893348a5b9880912
zhongxinyun <55012647+zhongxinyun@users.noreply.github.com>
2023-03-20 21:43:13 +0800

    上传微信定位解析文件和util文件

--- utils/util.js
+++ utils/util.js
@@ -1,26 +1,56 @@
 const formatTime = date => {
-    const year = date.getFullYear()
-    const month = date.getMonth() + 1
-    const day = date.getDate()
-    const hour = date.getHours()
-    const minute = date.getMinutes()
-    const second = date.getSeconds()
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
 
-    return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
+  return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
 }
 
 const formatNumber = n => {
-    n = n.toString()
-    return n[1] ? n : `0${n}`
-}
-const getNowDate = date => {
-    const year = date.getFullYear()
-    const month = date.getMonth() + 1
-    const day = date.getDate()
-    return [year, month, day].map(formatNumber).join('-')
+  n = n.toString()
+  return n[1] ? n : `0${n}`
 }
 
+const getWeekByDate = dates => {
+  let show_day = new Array('星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六');
+  let date = new Date(dates);
+  date.setDate(date.getDate());
+  let day = date.getDay();
+  return show_day[day];
+ }
+
+ const formatDate = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+
+  return `${[year, month, day].map(formatNumber).join('/')}`
+}
+
+const formatonlytime = date => {
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return `${[hour, minute, second].map(formatNumber).join(':')}`
+}
+
+const getNowDate = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  return [year, month, day].map(formatNumber).join('-')
+}
+
+
+
 module.exports = {
-    formatTime,
-    getNowDate: getNowDate
+  formatTime,
+  getWeekByDate,
+  formatDate,
+  formatonlytime,
+  getNowDate
 }
